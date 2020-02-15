c3df26bd8ea919b994c3b8a3c670053ce4220781
kile3r <gasljevic.kristijan@gmail.com>
2020-02-15 11:44:06 +0100

    Empty project

---
+++ becomeHabit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  becomeHabit
+//
+//  Created by Kristijan Gašljević on 15/02/2020.
+//  Copyright © 2020 Kristijan Gašljević. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
